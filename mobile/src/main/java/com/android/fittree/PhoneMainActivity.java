8b0a24a290aaf56e29bcc4c6ca31d65089e3fbec
BenedictLee <lee.2138@buckeyemail.osu.edu>
2014-10-05 02:21:27 -0400

    Working navigation bar

--- mobile/src/main/java/com/android/fittree/PhoneMainActivity.java
+++ mobile/src/main/java/com/android/fittree/PhoneMainActivity.java
@@ -2,12 +2,20 @@ package com.android.fittree;
 
 import android.app.Activity;
 import android.app.ActionBar;
+//import android.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -19,24 +27,67 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.app.Fragment;
+//import android.app.Fragment;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 
-public class PhoneMainActivity extends Activity {
+public class PhoneMainActivity extends FragmentActivity {
+
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private CharSequence mTitle;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_main);
-        if(savedInstanceState==null){
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+        setContentView(R.layout.activity_drawer);
+
+        mPlanetTitles=getResources().getStringArray(R.array.drawer_list);
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerList=(ListView)findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item,mPlanetTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                //super.onDrawerClosed(view);
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                //super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        if (savedInstanceState == null) {
+            selectItem(1);
         }
     }
+
     //keytool -list -v -alias androiddebugkey -keystore %%Your path%% -storepass android -keypass android
     @Override
     public boolean onCreateOptionsMenu(android.view.Menu menu) {
@@ -45,6 +96,12 @@ public class PhoneMainActivity extends Activity {
         MenuInflater blowup = getMenuInflater();
         blowup.inflate(R.menu.phone_main, menu);
         return true;
+    }
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
@@ -62,8 +119,55 @@ public class PhoneMainActivity extends Activity {
             Intent intent = new Intent("android.intent.action.PREFS");
             startActivity(intent);
         }
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // Create a new fragment and specify the planet to show based on position
+        Fragment fragment = new Fragment();
+        if(position == 1){
+            fragment = new PlaceholderFragment();
+        }
+        if(fragment != null){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+        }
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mPlanetTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
 
     /**
      * A placeholder fragment containing a simple view.
@@ -76,7 +180,7 @@ public class PhoneMainActivity extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            String[] data = {"Maps","Fitness","Calories", "Shit have yet to happen"};
+            String[] data = {"Maps","Fitness","Calories", "Lanciao"};
             List<String> Menu = new ArrayList<String>(Arrays.asList(data));
 
             mMenuAdapter = new ArrayAdapter<String>(
@@ -123,4 +227,5 @@ public class PhoneMainActivity extends Activity {
             Log.d(Log_tag,"Couldn't call "+ location + ",no such place");
         }
     }
+
 }
